ef7d7180184b6cb1f8cf79d5971571e6fd04008f
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2019-11-25 08:01:42 -0300

    Versão 1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
-  // Apply the Kotlin JVM plugin to add support for Kotlin.
   id("org.jetbrains.kotlin.jvm") version "1.3.50"
-  // Apply the java-library plugin for API and implementation separation.
-  `java-library`
 }
 
 repositories {
@@ -12,7 +9,7 @@ repositories {
 version = "1.0"
 
 dependencies {
-  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+  implementation("org.jetbrains.kotlin:kotlin-bom")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   testImplementation("org.jetbrains.kotlin:kotlin-test")
